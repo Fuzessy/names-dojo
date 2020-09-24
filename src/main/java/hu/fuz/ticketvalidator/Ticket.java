b90256f2a1f69ced10c0779129147129d34a29d4
Zsolt Füzesi <zsolt.fuzesi@idomsoft.hu>
2020-09-24 10:38:19 +0200

    recognise start time of the travel

--- src/main/java/hu/fuz/ticketvalidator/Ticket.java
+++ src/main/java/hu/fuz/ticketvalidator/Ticket.java
@@ -1,11 +1,15 @@
 package hu.fuz.ticketvalidator;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.regex.Pattern;
 
 public class Ticket {
-
+    private static final String METRO_LINE_DATE_PATTERN = "yyyyMMddHHmm";
+    private static final int NUMBER_OF_METRO_DATE_CHARACTERS = 9;
+    private static final int NUMBER_OF_DATE_CHARACTERS = 8;
     /* from 5th character appers 'xxx' cahacters */
-    private final String METRO_LINE_PATTERN = "^....xxx.*";
+    private static final String METRO_LINE_PATTERN = "^....xxx.*";
 
     private final String validationNumber;
 
@@ -16,5 +20,26 @@ public class Ticket {
     public boolean isMetroLine() {
         Pattern pattern = Pattern.compile(METRO_LINE_PATTERN);
         return pattern.matcher(validationNumber).find();
+    }
+
+    public LocalDateTime getTravelStartTime() {
+        if(isMetroLine()){
+            return determinateMetroTravelStartTime();
+        }
+        else{
+            return determineTravelStartTime();
+        }
+    }
+
+    private LocalDateTime determineTravelStartTime() {
+        String dateCode = validationNumber.substring(validationNumber.length() - NUMBER_OF_DATE_CHARACTERS);
+        dateCode = LocalDateTime.now().getYear() + dateCode;
+        return LocalDateTime.parse(dateCode, DateTimeFormatter.ofPattern(METRO_LINE_DATE_PATTERN));
+    }
+
+    private LocalDateTime determinateMetroTravelStartTime() {
+        String dateCode = validationNumber.substring(validationNumber.length() - NUMBER_OF_METRO_DATE_CHARACTERS);
+        dateCode = "202" + dateCode;
+        return LocalDateTime.parse(dateCode, DateTimeFormatter.ofPattern(METRO_LINE_DATE_PATTERN));
     }
 }
--- src/test/java/hu/fuz/TicketTest.java
+++ src/test/java/hu/fuz/TicketTest.java
@@ -3,24 +3,38 @@ package hu.fuz;
 import hu.fuz.ticketvalidator.Ticket;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
 
 public class TicketTest {
 
-    final private String METRO_VALIDATION_CODE = "0643xxx911281305";
-    final private String NOT_METRO_VALIDATION_CODE = "5422511281305";
-
     @Test
     public void isMetroLineTest(){
+        String METRO_VALIDATION_CODE = "0643xxx911281305";
         Ticket ticket = new Ticket(METRO_VALIDATION_CODE);
         assertTrue(ticket.isMetroLine());
     }
 
     @Test
     public void isNotMetroLineTest(){
+        String NOT_METRO_VALIDATION_CODE = "5422512041246";
         Ticket ticket = new Ticket(NOT_METRO_VALIDATION_CODE);
         assertFalse(ticket.isMetroLine());
+    }
+
+    @Test
+    public void getMetroTravelStartDateTest(){
+        String METRO_VALIDATION_CODE_START_20200924_1305 = "0643xxx009241305";
+        Ticket ticket = new Ticket(METRO_VALIDATION_CODE_START_20200924_1305);
+        assertEquals(LocalDateTime.of(2020,9,24,13,5), ticket.getTravelStartTime());
+    }
+
+    @Test
+    public void getNotMetroTravelStartDateTest(){
+        String METRO_VALIDATION_CODE_START_20200924_1305 = "5422512041246";
+        Ticket ticket = new Ticket(METRO_VALIDATION_CODE_START_20200924_1305);
+        assertEquals(LocalDateTime.of(2020,12,4,12,46), ticket.getTravelStartTime());
     }
 
 }
